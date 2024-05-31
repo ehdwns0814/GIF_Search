9de8fc4a601ac2af5c980344fabcc78518e41433
DongJun96 <djlee1201@naver.com>
2024-05-31 20:50:57 +0900

    feat: SearchViewModel 구현

---
+++ GIF_Search/Presentation/Search/ViewModels/SearchViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  SearchViewModel.swift
+//  GIF_Search
+//
+//  Created by 동준 on 5/27/24.
+//
+
+import Foundation
+
+final class SearchViewModel {
+    typealias T = ImageInfoViewModel
+    
+    var gifURLStorage: Observable<[T]> = Observable([])
+    
+    private let giphyService: GiphyService
+    
+    private var hasNextPage: Bool?
+    private var currentOffset: Int?
+    private var searchType: MediaType = .gif
+    private var searchQuery: String?
+    
+    
+    // MARK: - Initializer(s)
+    
+    init(giphyService: GiphyService) {
+        self.giphyService = giphyService
+    }
+    
+    // MARK: - Method(s)
+    
+    func searchedFor(text: String) {
+        self.searchQuery = text
+    }
+    
+    private func searchGif(type: MediaType, query: String, offset: Int = 0) {
+        self.giphyService.searchGif(
+            type: type,
+            query: query,
+            offset: offset) { [weak self] result in
+                switch result {
+                case .success(let gifBundle):
+                    self?.hasNextPage = gifBundle.hasNextPage
+                    self?.currentOffset = gifBundle.offset + 10
+                    self?.gifURLStorage.value? += gifBundle.gifs.map { ImageInfoViewModel(image: $0) }
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+}
