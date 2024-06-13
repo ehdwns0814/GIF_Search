6dd59d6035e771e53cd349d1f527f1d788283153
DongJun96 <djlee1201@naver.com>
2024-06-13 12:26:56 +0900

    feat: GifBundle 구현

---
+++ GIF_Search/New Group/Model/GifBundle.swift
@@ -0,0 +1,14 @@
+//
+//  GifBundle.swift
+//  GIF_Search
+//
+//  Created by 동준 on 5/26/24.
+//
+
+import Foundation
+
+struct GifBundle {
+    let offset: Int
+    let hasNextPage: Bool
+    let gifs: [Gif]
+}
